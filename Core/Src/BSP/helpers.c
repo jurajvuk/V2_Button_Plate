4349f3274a0753cf40f791cc98a99c44e1b67f92
Juraj Vukić <jurajvuk@gmail.com>
2026-03-29 19:40:51 +0200

    nis posebno

--- Core/Src/BSP/helpers.c
+++ Core/Src/BSP/helpers.c
@@ -3,6 +3,7 @@
 /*********************************INCLUDES******************************************/
 #include "BSP/helpers.h"
 #include "BSP/usb_hid_report.h"
+#include "stm32f1xx_it.h"
 #include <stdint.h>
 
 /**********************GLOBAL VARIABLES AND CONSTANTS*******************************/
@@ -13,11 +14,12 @@
 /*********************************FUNCTIONS*****************************************/
 void assign_inputs_to_HID_structure(uint8_t inputs[3], uint8_t offset)
 {
-    for (int i = 0; i < 2; i++) {
-        if (i == 0) {
-          inputs[i] |= ~0xFE;
-        }
-        // Invert because buttons are active-low (pressed=0, released=1)
-        HID_InputReport.buttons[offset + i] = ~inputs[i];
+  
+  for (int i = 0; i < 2; i++) {
+      if (i == 0) {
+        inputs[i] |= ~0xFE;
       }
+      // Invert because buttons are active-low (pressed=0, released=1)
+      HID_InputReport.buttons[offset + i] = ~inputs[i];
+  }
 }
